5a34b7d6a66a9dd8e9e799cf0d2f882d4eabc337
oitns106 <oitns106@gmail.com>
2022-09-09 23:59:33 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/oitns106/b1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.oitns106.b1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/demo.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class Demo extends StatefulWidget {
+  @override
+  _DemoState createState() => _DemoState();
+}
+
+class _DemoState extends State<Demo> {
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+  double? _result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _heightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'height in cm',
+                icon: Icon(Icons.trending_up),
+              ),
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: _weightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'weight in kg',
+                icon: Icon(Icons.line_weight),
+              ),
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              //color: Colors.pinkAccent,
+              child: Text(
+                "計算",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              onPressed: calculateBMI,
+            ),
+            SizedBox(height: 15),
+            Text(
+              _result == null ? "" : "您的BMI=${_result?.toStringAsFixed(2)}",
+              style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void calculateBMI() {
+    double height = double.parse(_heightController.text) / 100;
+    double weight = double.parse(_weightController.text);
+
+    double heightSquare = height * height;
+    double result = weight / heightSquare;
+    _result = result;
+    setState(() {});
+  }
+}
